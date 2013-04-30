68594187cfea23fffb47c2e0f06671867e5ae505
Skala <skala.m@husky.neu.edu>
2013-04-29 18:00:39 -0700

    Java

---
+++ historyReportGenerator.java
@@ -0,0 +1,9 @@
+// History Report Generator
+// idea stolen from James Larisch -- this is merely an exercise
+
+// goal: program takes search term input, searches Wikipedia for this
+// input and returns the first paragraph of the first article returned
+// by the search. A quick and easy way to learn about an unknown topic.
+// Disclaimer: do not actually turn in a history report created using
+// this program
+
