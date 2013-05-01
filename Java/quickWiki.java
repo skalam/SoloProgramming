9341c9d424463c9fedbf64a5f5708b69a6d9aeff
Skala <skala.m@husky.neu.edu>
2013-05-01 13:25:57 -0700

    Removed old stuff

--- Java/quickWiki.java
+++
@@ -1,6 +0,0 @@
-// Quick Wiki
-// idea stolen from James Larisch -- this is merely an exercise
-
-// goal: program takes search term input, searches Wikipedia for this
-// input and returns the first paragraph of the first article returned
-// by the search. A quick and easy way to learn about an unknown topic.
